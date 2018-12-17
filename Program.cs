268f94acfbe9245e57985b54e8a12ada3caa345d
FearDog <FearDog@bk.ru>
2018-12-17 09:34:25 +0500

    Змейка стала управляемой

--- Program.cs
+++ Program.cs
@@ -28,25 +28,17 @@ namespace SnakeGame
       Point p = new Point(4, 5, '*');
       Snake snake = new Snake(p, 4, Direction.RIGTH);
       snake.Draw();
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
-      Thread.Sleep(300);
-      snake.Move();
+
+      while(true)
+      {
+        if(Console.KeyAvailable)
+        {
+          ConsoleKeyInfo key = Console.ReadKey();
+          snake.HandleKey(key.Key);
+        }
+        Thread.Sleep(100);
+        snake.Move();
+      }
 
       Console.ReadLine();
     }
